9af3b449af110fdd94e6b2d2ce83eb606a9e2829
johnpsom <52068616+johnpsom@users.noreply.github.com>
2021-12-13 11:42:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -91,8 +91,8 @@ st.markdown('''
 #### **Beware** that this app is created for educational and informational purposes only. 
 #### Stock markets are volatile and there is risk involved and also loss of money.
 #### The creator of this app is in no case responsible of what you do with your money. 
-#### Comments are welcome and accepted in the article's page on [medium.com](medium.com) or email me at <getyour.portfolio@gmail.com>.
-#### Check article on [medium.com](medium.com) for instructions and guidance on what this app does and how it works.
+#### Comments are welcome and accepted in the article's page on [medium.com](https://www.medium.com) or email me at <getyour.portfolio@gmail.com>.
+#### Check article on [medium.com](https://www.medium.com) for instructions and guidance on what this app does and how it works.
 
 #### Choose values for the parameters in the sidebar accordingly and check out the portfolio
 #### given as result.
